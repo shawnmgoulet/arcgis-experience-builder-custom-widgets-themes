5fb2c4624e29e2aee2f3fc67e928dd814fc4e913
shawnmgoulet <3179974+shawnmgoulet@users.noreply.github.com>
2021-09-28 10:53:47 -0400

    Add sample custom widgets, a custom theme, update README.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# arcgis-experience-builder-custom-widgets-themes
+# ArcGIS Experience Builder Custom Widgets & Themes Repo
+
 An Experience Builder custom widgets and themes sample repo.
+
+## What's important
+
+1. The key to the repo is that at it's root, it has a `manifest.json` file. The below properties (within this repo's `manifest.json`) are necessary for your repo to be integrated in your development environment's `client` directory and its webpack compile. Comments pertaining to necessary properties and values are included below.
+
+```json
+{
+  "name": "exb-custom-widgets-themes-repo", // Required property
+  "type": "exb-web-extension-repo", // Required property/value for the repo to be compiled by webpack within client
+  "description": "This is a sample custom widgets and themes repo for the developer edition of ArcGIS Experience Builder.", // Required property
+  "copyright": "", // Required property
+  "license": "http://www.apache.org/licenses/LICENSE-2.0" // Required property
+}
+```
+
+2. Within your repo, any custom widgets must live within a `widgets` child directory. Similarly, any custom themes must live within a `themes` child directory.
+
+3.
---
+++ widgets/demo-function-copy/src/runtime/widget.tsx
@@ -0,0 +1,47 @@
+/**
+  Licensing
+
+  Copyright 2021 Esri
+
+  Licensed under the Apache License, Version 2.0 (the "License"); You
+  may not use this file except in compliance with the License. You may
+  obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+  implied. See the License for the specific language governing
+  permissions and limitations under the License.
+
+  A copy of the license is available in the repository's
+  LICENSE file.
+*/
+import { AllWidgetProps, React, IMState, FormattedMessage } from 'jimu-core';
+import { IMConfig } from '../config';
+import defaultMessage from './translations/default';
+
+interface ExtraProps {
+  locale: string;
+}
+
+export default function Widget(props: AllWidgetProps<IMConfig> & ExtraProps) {
+  return <div className="widget-demo-function-copy jimu-widget" style={{ overflow: 'auto' }}>
+    This widget demonstrates creating a functional component.
+    <div>
+      config: {JSON.stringify(props.config)}
+    </div>
+    <div>
+      locale: {props.locale}
+    </div>
+    <div>
+      i18n: <FormattedMessage id="str1" defaultMessage={defaultMessage.str1}></FormattedMessage>
+    </div>
+  </div>;
+}
+
+Widget.mapExtraStateProps = (state: IMState, ownProps: AllWidgetProps<IMConfig>): ExtraProps => {
+  return {
+    locale: state.appContext.locale
+  }
+}
